6b41df077b4b903e9f3ce1af1930fc9b9f763928
MohammedJaber93 <mohjbr.dev@gmail.com>
2022-10-28 23:55:48 +0300

    Use the new LoginForm component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import { Input } from "components";
-import { RegistrationCard } from "features/authentication";
+import { RegistrationCard, LoginForm } from "features/authentication";
 import type { NextPageWithLayout } from "types";
 
 const Login: NextPageWithLayout = () => {
@@ -16,19 +15,7 @@ const Login: NextPageWithLayout = () => {
         </>
       }
     >
-      <form>
-        <Input label="Email" inputClassName="mb-[35px]" />
-        <Input label="Password" type="password" />
-        <Link href="#" className="block text-sm text-gray-dark text-right">
-          Forgot Password?
-        </Link>
-        <button
-          type="submit"
-          className="block w-full text-2xl bg-blue-light text-white rounded-lg p-[14px] mt-16"
-        >
-          Sign In
-        </button>
-      </form>
+      <LoginForm />
     </RegistrationCard>
   );
 };
