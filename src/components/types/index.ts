25799da5e684802d1aac4589f871796e2022803b
MohammedJaber93 <mohjbr.dev@gmail.com>
2022-10-28 22:52:56 +0300

    Export all the interfaces + remove RegistrationCardType

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -3,10 +3,10 @@ import { ImageProps } from "next/image";
 
 type DivElementType = HTMLProps<HTMLDivElement>;
 
-interface CardProps extends DivElementType {}
+export interface CardProps extends DivElementType {}
 export type CardType = FC<CardProps>;
 
-interface InputProps extends HTMLProps<HTMLInputElement> {
+export interface InputProps extends HTMLProps<HTMLInputElement> {
   label?: string;
   helperText?: string;
   inputClassName?: string;
@@ -14,10 +14,8 @@ interface InputProps extends HTMLProps<HTMLInputElement> {
 }
 export type InputType = FC<InputProps>;
 
-interface LogoProps extends Omit<ImageProps, "src" | "alt"> {
+export interface LogoProps extends Omit<ImageProps, "src" | "alt"> {
   src?: string;
   alt?: string;
 }
 export type LogoType = FC<LogoProps>;
-
-export type RegistrationCardType = CardType;
