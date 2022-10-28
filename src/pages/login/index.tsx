117311c534330c9eb1b3e395144862727f09b34f
MohammedJaber93 <mohjbr.dev@gmail.com>
2022-10-28 23:37:57 +0300

    Use RegistrationCard component inside Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,38 +1,38 @@
 import Link from "next/link";
-import { Card, Input, Logo } from "components";
+import { Input } from "components";
+import { RegistrationCard } from "features/authentication";
 import type { NextPageWithLayout } from "types";
 
 const Login: NextPageWithLayout = () => {
   return (
-    <Card className="w-full sm:w-[703px] font-semibold">
-      <Logo className="m-auto" />
-      <h1 className="text-3xl text-center">Talents Valley</h1>
-      <div className="sm:max-w-[500px] m-auto">
-        <p className="text-2xl mt-[55px] mb-[42px]">Login to Your Account</p>
-        <form>
-          <Input label="Email" inputClassName="mb-[35px]" />
-          <Input label="Password" type="password" />
-          <Link
-            href="#"
-            className="block text-base text-gray-dark font-normal text-right"
-          >
-            Forgot Password?
+    <RegistrationCard
+      formTitle="Login to Your Account"
+      formCaption={
+        <>
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="text-blue-light">
+            Sign up
           </Link>
-          <button
-            type="submit"
-            className="block w-full text-2xl bg-blue-light text-white rounded-lg p-[14px] mt-16"
-          >
-            Sign In
-          </button>
-        </form>
-      </div>
-      <p className="text-base text-center font-normal mt-12 mb-24">
-        Don&apos;t have an account?{" "}
-        <Link href="#" className="text-blue-light">
-          Sign up
+        </>
+      }
+    >
+      <form>
+        <Input label="Email" inputClassName="mb-[35px]" />
+        <Input label="Password" type="password" />
+        <Link
+          href="#"
+          className="block text-base text-gray-dark font-normal text-right"
+        >
+          Forgot Password?
         </Link>
-      </p>
-    </Card>
+        <button
+          type="submit"
+          className="block w-full text-2xl bg-blue-light text-white rounded-lg p-[14px] mt-16"
+        >
+          Sign In
+        </button>
+      </form>
+    </RegistrationCard>
   );
 };
 
