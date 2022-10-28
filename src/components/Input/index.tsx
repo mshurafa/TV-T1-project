7353acdb09e2db686b122e5be95e0b784ae46b92
MohammedJaber93 <mohjbr.dev@gmail.com>
2022-10-28 23:29:42 +0300

    Adjust Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,8 @@
+import { useMemo } from "react";
 import type { InputType } from "components/types";
 
 export const Input: InputType = ({
   label,
-  placeholder,
   id,
   helperText,
   size,
@@ -11,27 +11,29 @@ export const Input: InputType = ({
   startIcon,
   ...rest
 }) => {
+  const classNames = useMemo(
+    () => ({
+      inputContainer: `mb-2 relative ${className ?? ""}`,
+      label: "block mb-2 text-lg text-gray-dark",
+      startIcon: "absolute left-3 top-1/2 -translate-y-2/4",
+      input: `block w-full px-4 py-[19px] text-base leading-5 font-normal text-gray-dark outline-none border border-gray focus:border-blue rounded-lg ${
+        inputClassName || ""
+      }`,
+      helperText: "text-sm text-gray-500",
+    }),
+    [className, inputClassName]
+  );
+
   return (
-    <div className={`mb-2 relative ${className || ""}`}>
+    <div className={classNames.inputContainer}>
       {label && (
-        <label htmlFor={id} className="block mb-2 text-lg text-gray-dark">
+        <label htmlFor={id} className={classNames.label}>
           {label}
         </label>
       )}
-      {startIcon && (
-        <span className="absolute left-3 top-1/2 -translate-y-2/4">
-          {startIcon}
-        </span>
-      )}
-      <input
-        id={id}
-        placeholder={placeholder}
-        className={`block w-full px-4 py-[19px] text-base leading-5 font-normal text-gray-dark outline-none border border-gray focus:border-blue rounded-lg ${
-          inputClassName || ""
-        }`}
-        {...rest}
-      />
-      <p className="text-sm text-gray-500">{helperText}</p>
+      {startIcon && <span className={classNames.startIcon}>{startIcon}</span>}
+      <input id={id} className={classNames.input} {...rest} />
+      {helperText && <p className={classNames.helperText}>{helperText}</p>}
     </div>
   );
 };
