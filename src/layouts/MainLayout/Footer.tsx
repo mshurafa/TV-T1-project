cbb2d44c34b0f2a53ecc6c6076508a808d52b47d
MohammedJaber93 <mohjbr.dev@gmail.com>
2022-10-29 01:11:03 +0300

    Adjust footer styles

--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white flex justify-evenly items-center py-10 px-3 text-xs sm:pl-28 sm:justify-start sm:text-sm">
+    <footer className="bg-white flex justify-evenly items-center py-6 px-3 text-xs sm:px-12 sm:justify-start sm:text-sm">
       <Link href="#">Talents Valley</Link>
       <Link href="#" className="px-3 sm:px-16">
         Contacts
