57211fbdcb4cff64a542d84c9f83185c5dc7ad34
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-04 21:47:43 +0200

    Create wrapper file for the @heroicons package

---
+++ src/lib/@heroicons/index.ts
@@ -0,0 +1,12 @@
+import {
+  ExclamationTriangleIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/20/solid";
+import { EyeSlashIcon, EyeIcon } from "@heroicons/react/24/solid";
+
+export {
+  ExclamationTriangleIcon as ErrorIcon,
+  CheckCircleIcon,
+  EyeSlashIcon,
+  EyeIcon,
+};
