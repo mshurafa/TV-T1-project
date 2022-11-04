e3bdc49dec8d3efe30b878c94f1f57204d3964b9
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-04 22:48:57 +0200

    Create ForgotPassword page

---
+++ src/pages/forgot-password/index.tsx
@@ -0,0 +1,21 @@
+import { RegistrationCard, ForgotPasswordForm } from "features/authentication";
+import type { NextPageWithLayout } from "types";
+
+const ForgotPassword: NextPageWithLayout = () => {
+  return (
+    <RegistrationCard formTitle="Forgot Password?">
+      <p className="text-sm text-gray-dark -mt-6 mb-9">
+        We&apos;ll send a code to your email to reset your password
+      </p>
+      <ForgotPasswordForm />
+    </RegistrationCard>
+  );
+};
+
+ForgotPassword.mainLayoutProps = {
+  title: "Talents Valley Forgot Password",
+  pageDescription: "Forgot password page description",
+  withoutNavbar: true,
+};
+
+export default ForgotPassword;
