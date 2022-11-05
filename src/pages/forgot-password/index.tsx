d5b8dbb63b224956b5fe449d402719cedac9caa2
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-06 00:42:26 +0200

    Use NoSsr component for ForgotPassword page

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,14 +1,17 @@
+import { NoSsr } from "components";
 import { RegistrationCard, ForgotPasswordForm } from "features/authentication";
 import type { NextPageWithLayout } from "types";
 
 const ForgotPassword: NextPageWithLayout = () => {
   return (
-    <RegistrationCard formTitle="Forgot Password?" withBackButton>
-      <p className="text-sm text-gray-dark -mt-6 mb-9">
-        We&apos;ll send a code to your email to reset your password
-      </p>
-      <ForgotPasswordForm />
-    </RegistrationCard>
+    <NoSsr>
+      <RegistrationCard formTitle="Forgot Password?" withBackButton>
+        <p className="text-sm text-gray-dark -mt-6 mb-9">
+          We&apos;ll send a code to your email to reset your password
+        </p>
+        <ForgotPasswordForm />
+      </RegistrationCard>
+    </NoSsr>
   );
 };
 
