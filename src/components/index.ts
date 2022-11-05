64ee7800761dd476918f14dc9834f4da30871fee
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-06 00:38:22 +0200

    Create NoSsr custom component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+import NoSsr from "./NoSsr";
+
 export * from "./Card";
 export * from "./Input";
 export * from "./Logo";
@@ -5,3 +7,4 @@ export * from "./Button";
 export * from "./Select";
 export * from "./HelperText";
 export * from "./IconButton";
+export { NoSsr };
