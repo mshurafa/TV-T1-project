bf65ee29a4059c36a406060b4441f93abbd40238
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-13 00:19:54 +0200

    Remove footer white background color

--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white flex justify-evenly items-center py-6 px-3 text-xs sm:px-12 sm:justify-start sm:text-sm">
+    <footer className="flex justify-evenly items-center py-6 px-3 text-xs sm:px-12 sm:justify-start sm:text-sm">
       <Link href="#">Talents Valley</Link>
       <Link href="#" className="px-3 sm:px-16">
         Contacts
