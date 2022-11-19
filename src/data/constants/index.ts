a780b350d3547127d5efdb5c8c84846f53976576
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-19 23:02:28 +0200

    Create Global constants folder + create API_SERVICES_URLS

---
+++ src/data/constants/index.ts
@@ -0,0 +1,4 @@
+export const API_SERVICES_URLS = {
+  SIGN_UP: "/user/signup",
+  SIGN_IN: "/user/login",
+} as const;
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ import countriesList from "./json/countriesList.json";
 
 export { countriesList };
 export * from "./config";
+export * from "./constants";
