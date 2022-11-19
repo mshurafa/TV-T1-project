3f45ea1dfbd0b7fc48226ff4372e3b0f27a2d63d
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-20 01:01:36 +0200

    Create URL_PATHS constant

--- src/data/constants/index.ts
+++ src/data/constants/index.ts
@@ -2,3 +2,12 @@ export const API_SERVICES_URLS = {
   SIGN_UP: "/user/signup",
   SIGN_IN: "/user/login",
 } as const;
+
+export const URL_PATHS = {
+  HOME: "/",
+  AUTH: {
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    FORGOT_PASSWORD: "/forgot-password",
+  },
+} as const;
