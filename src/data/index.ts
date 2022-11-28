b0f2d905bede3542cb495edc325098445e024947
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-28 20:38:31 +0200

    Create routes folder

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,3 +3,4 @@ import countriesList from "./json/countriesList.json";
 export { countriesList };
 export * from "./config";
 export * from "./constants";
+export * from "./routes";
---
+++ src/data/routes/index.ts
@@ -0,0 +1,22 @@
+export const URL_PATHS = {
+  HOME: "/",
+  AUTH: {
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    FORGOT_PASSWORD: "/forgot-password",
+  },
+} as const;
+
+export const APP_ROUTES: {
+  readonly PROTECTED_ROUTES: readonly string[];
+  readonly PUBLIC_ROUTES: readonly string[];
+  readonly AUTH_ROUTES: readonly string[];
+} = {
+  PROTECTED_ROUTES: [URL_PATHS.HOME],
+  PUBLIC_ROUTES: [],
+  AUTH_ROUTES: [
+    URL_PATHS.AUTH.SIGN_IN,
+    URL_PATHS.AUTH.SIGN_UP,
+    URL_PATHS.AUTH.FORGOT_PASSWORD,
+  ],
+};
