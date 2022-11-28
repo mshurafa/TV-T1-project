fea78a54472b6490360064b0a41f331ee23e46e0
MohJaber93 <mohjbr.dev@gmail.com>
2022-11-29 01:05:39 +0200

    Add VERIFY_CODE url path

--- src/data/routes/index.ts
+++ src/data/routes/index.ts
@@ -4,6 +4,7 @@ export const URL_PATHS = {
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
     FORGOT_PASSWORD: "/forgot-password",
+    VERIFY_CODE: "/forgot-password/verify-code",
   },
 } as const;
 
