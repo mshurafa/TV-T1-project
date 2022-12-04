2f08023f8b5eaa7f83c5a91d9161919b656d3223
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-05 01:13:30 +0200

    Align footer in the center

--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "components";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-evenly items-center py-6 px-3 text-xs sm:px-12 sm:justify-start sm:text-sm">
+    <footer className="flex justify-evenly items-center py-6 px-3 text-xs sm:px-12 sm:justify-center sm:text-sm">
       <Link href="#">Talents Valley</Link>
       <Link href="#" className="px-3 sm:px-16">
         Contacts
