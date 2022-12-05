8adf9fe4d51a48d8b51a79c5d7b6ae269ac423e0
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-06 01:28:26 +0200

    Add VERIFY_CODE url to API_SERVICES_URLS constant

--- src/data/constants/index.ts
+++ src/data/constants/index.ts
@@ -2,6 +2,7 @@ export const API_SERVICES_URLS = {
   SIGN_UP: "/user/signup",
   SIGN_IN: "/user/login",
   FORGOT_PASSWORD: "/user/password/forgot",
+  VERIFY_CODE: "/user/password/verify-code",
 } as const;
 
 export const COOKIES_KEYS = {
