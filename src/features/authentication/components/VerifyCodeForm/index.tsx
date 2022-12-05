684f8f80b1282d9c8b97fa5684f7ef3f5bc9f641
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-06 01:51:12 +0200

    Finalize VerifyCodeForm component logic

--- src/features/authentication/components/VerifyCodeForm/index.tsx
+++ src/features/authentication/components/VerifyCodeForm/index.tsx
@@ -1,34 +1,63 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Button, HelperText } from "components";
-import OtpInput from "../OtpInput";
+import { useAxios } from "hooks";
+import { API_SERVICES_URLS, URL_PATHS, COOKIES_KEYS } from "data";
 import { ErrorIconMini } from "lib/@heroicons";
+import OtpInput from "../OtpInput";
+import type {
+  VerifyCodeFormPayloadType,
+  VerifyCodeResponseType,
+} from "../../types";
 
 export const VerifyCodeForm = () => {
   const [otpCode, setOtpCode] = useState("");
-  console.log(
-    "🚀 ~ file: index.tsx ~ line 9 ~ VerifyCodeForm ~ otpCode",
-    otpCode
-  );
-  const [error, setError] = useState("");
   const router = useRouter();
+  const {
+    fetchData: verifyCode,
+    error,
+    loading,
+  } = useAxios<VerifyCodeResponseType, VerifyCodeFormPayloadType>({
+    config: {
+      url: API_SERVICES_URLS.VERIFY_CODE,
+      method: "POST",
+    },
+    options: {
+      manual: true,
+    },
+    onSuccess: (data) => {
+      router.push(
+        {
+          pathname: URL_PATHS.AUTH.NEW_PASSWORD,
+          query: {
+            recoverToken: data.data?.recoverToken,
+          },
+        },
+        URL_PATHS.AUTH.NEW_PASSWORD
+      );
+    },
+  });
 
   const otpChangeHandler = (value: string) => setOtpCode(value);
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (loading) return;
+    const payload = {
+      _id: router.query.forgotPasswordData?.[1] || "",
+      verificationCode: otpCode,
+    };
+    verifyCode(payload);
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        setError((e) => (e ? "" : "Incorrect code"));
-        // router.push("verified");
-      }}
-    >
+    <form onSubmit={onSubmit}>
       <OtpInput onOtpChange={otpChangeHandler} />
       <HelperText
         showContent={!!error}
         className="text-red w-full text-xs justify-center min-h-[20px]"
         startIcon={<ErrorIconMini className="w-5 h5" />}
-        text={error}
+        text={error?.message}
       />
       <Button type="submit" buttonSize="small" fullWidth>
         Continue
