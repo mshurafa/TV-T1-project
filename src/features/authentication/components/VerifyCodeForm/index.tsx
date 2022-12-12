690a27724f2279a2a8478f958ca9c0033529eb3b
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-12 22:50:21 +0200

    Remove unused code

--- src/features/authentication/components/VerifyCodeForm/index.tsx
+++ src/features/authentication/components/VerifyCodeForm/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { Button, HelperText } from "components";
 import { useAxios } from "hooks";
-import { API_SERVICES_URLS, URL_PATHS, COOKIES_KEYS } from "data";
+import { API_SERVICES_URLS, URL_PATHS } from "data";
 import { ErrorIconMini } from "lib/@heroicons";
 import OtpInput from "../OtpInput";
 import type {
