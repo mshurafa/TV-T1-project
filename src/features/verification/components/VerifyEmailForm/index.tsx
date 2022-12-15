1e8488954f67af4677f99cc787b07342dfe96fd4
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-16 00:53:32 +0200

    Finish VerifyEmailForm component

--- src/features/verification/components/VerifyEmailForm/index.tsx
+++ src/features/verification/components/VerifyEmailForm/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Button, HelperText, OtpInput } from "components";
+import { useCurrentUser } from "features/authentication";
 import { useAxios } from "hooks";
 import { API_SERVICES_URLS, URL_PATHS } from "data";
 import { ErrorIconMini } from "lib/@heroicons";
+import { useEmailCode } from "../../hooks";
 import type {
   VerifyEmailFormPayloadType,
   VerifyEmailResponseType,
@@ -12,6 +14,8 @@ import type {
 export const VerifyEmailForm = () => {
   const [otpCode, setOtpCode] = useState("");
   const router = useRouter();
+  const { user } = useCurrentUser();
+  const { sendCodeRequest, loading: emailCodeLoading } = useEmailCode();
   const {
     fetchData: verifyCode,
     error,
@@ -32,6 +36,14 @@ export const VerifyEmailForm = () => {
     },
   });
 
+  if (user?.verifiedEmail) {
+    return (
+      <p className="text-sm text-center text-gray-dark">
+        Your email has been verified
+      </p>
+    );
+  }
+
   const otpChangeHandler = (value: string) => setOtpCode(value);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -41,18 +53,32 @@ export const VerifyEmailForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <OtpInput onOtpChange={otpChangeHandler} />
-      <HelperText
-        showContent={!!error}
-        className="text-red w-full text-xs justify-center min-h-[20px]"
-        startIcon={<ErrorIconMini className="w-5 h5" />}
-        text={error?.message}
-      />
-      <Button type="submit" buttonSize="small" fullWidth>
-        Continue
-      </Button>
-    </form>
+    <>
+      <p className="text-sm text-gray-dark mb-4">
+        {`We have sent you a verification code to your email ${user?.email}`}
+      </p>
+      <form onSubmit={onSubmit}>
+        <OtpInput onOtpChange={otpChangeHandler} />
+        <HelperText
+          showContent={!!error}
+          className="text-red w-full text-xs justify-center min-h-[20px]"
+          startIcon={<ErrorIconMini className="w-5 h5" />}
+          text={error?.message}
+        />
+        <Button type="submit" buttonSize="small" fullWidth>
+          Continue
+        </Button>
+      </form>
+      <p className="text-sm text-center mt-8">
+        Didn&apos;t get the code?{" "}
+        <a
+          onClick={() => sendCodeRequest()}
+          className="text-blue-light cursor-pointer"
+        >
+          {emailCodeLoading ? "Loading..." : "Resend"}
+        </a>
+      </p>
+    </>
   );
 };
 
