671b1d7da54dc9c107dee287dcaacefcf070cdec
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-17 00:26:08 +0200

    Finish EmailVerification page

--- src/pages/verification/email.tsx
+++ src/pages/verification/email.tsx
@@ -1,15 +1,56 @@
-import { VerificationCard, VerifyEmailForm } from "features/verification";
+import { useState } from "react";
+import {
+  VerificationCard,
+  VerifyEmailForm,
+  VerifiedSuccess,
+  useEmailCode,
+} from "features/verification";
+import { useCurrentUser } from "features/authentication";
 import { NoSsr } from "components";
 import type { NextPageWithLayout } from "types";
 
 const EmailVerification: NextPageWithLayout = () => {
+  const { user } = useCurrentUser();
+  const [isVerified, setIsVerified] = useState(user?.verifiedEmail);
+  const { sendCodeRequest, loading } = useEmailCode();
+
+  let imgSrc = "/assets/img/email.png";
+  let caption: React.ReactNode = (
+    <>
+      Didn&apos;t get the code?{" "}
+      <a
+        onClick={() => sendCodeRequest()}
+        className="text-blue-light cursor-pointer"
+      >
+        {loading ? "Loading..." : "Resend"}
+      </a>
+    </>
+  );
+  let content = (
+    <>
+      <p className="text-sm text-gray-dark mb-4">
+        {`We have sent you a verification code to your email ${user?.email}`}
+      </p>
+      <VerifyEmailForm onVerify={() => setIsVerified(true)} />
+    </>
+  );
+
+  if (isVerified) {
+    imgSrc = "/assets/img/check-mark.png";
+    caption = null;
+    content = (
+      <VerifiedSuccess description="Your Email has been Verified Successfully" />
+    );
+  }
+
   return (
     <NoSsr>
       <VerificationCard
         title="Email Verification"
-        imgSrc="/assets/img/email.png"
+        imgSrc={imgSrc}
+        caption={caption}
       >
-        <VerifyEmailForm />
+        {content}
       </VerificationCard>
     </NoSsr>
   );
