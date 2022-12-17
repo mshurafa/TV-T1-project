5e5eca60625164b17e5a915dc7b142869570b022
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-18 01:31:03 +0200

    Use VALIDATION_RULES from the root data folder

--- src/features/authentication/data/validation/index.ts
+++ src/features/authentication/data/validation/index.ts
@@ -1,9 +1,4 @@
-const VALIDATION_RULES = {
-  isEmail:
-    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-  password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/,
-  isNumber: /^\d+$/,
-} as const;
+import { VALIDATION_RULES } from "data";
 
 export const formValidation = {
   fullName: {
