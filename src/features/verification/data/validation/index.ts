ffaad47aa17bd4d105c54d0fce12307ce5b7e134
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-18 23:57:05 +0200

    Adjust formValidation for verification feature

--- src/features/verification/data/validation/index.ts
+++ src/features/verification/data/validation/index.ts
@@ -8,10 +8,14 @@ export const formValidation = {
     required: "ID Number is required",
     pattern: {
       value: VALIDATION_RULES.isNumber,
-      message: "ID number should be a number",
+      message: "Should be a number",
     },
   },
   file: {
     required: "File is required",
+    validate: (value: FileList) => {
+      const is2MBMax = value[0].size <= 2000000;
+      return is2MBMax || "Your file is too big must be 2 MP maximum";
+    },
   },
 } as const;
