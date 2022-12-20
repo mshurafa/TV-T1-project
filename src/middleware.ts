7e1675eacc58d255a5d8d6407bd827c4aa996705
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-20 20:48:56 +0200

    Use the new middlewareHandler inside middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,9 @@
 import type { NextRequest } from "next/server";
 import { COOKIES_KEYS } from "data";
-import { routesMiddleware } from "utils";
-import type { CurrentUserType } from "types";
+import { middlewareHandler } from "utils";
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get(COOKIES_KEYS.currentUser);
-  let currentUserJson: CurrentUserType | null = null;
-  if (currentUser) {
-    currentUserJson = JSON.parse(currentUser || '""');
-  }
-  let userDate = currentUserJson?.user;
-  const isLoggedIn = !!(
-    currentUserJson?.accessToken &&
-    currentUserJson.refreshToken &&
-    userDate
-  );
 
-  return routesMiddleware(request, isLoggedIn);
+  return middlewareHandler(request, currentUser);
 }
