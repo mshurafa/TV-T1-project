19b27e59e766aa567d6bad32ef2ba0a4f1b297c9
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-22 01:42:22 +0200

    Use the default import for middlewareHandler

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from "next/server";
 import { COOKIES_KEYS } from "data";
-import { middlewareHandler } from "utils";
+import middlewareHandler from "utils/middlewareHandler";
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get(COOKIES_KEYS.currentUser);
