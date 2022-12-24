66c66fac6be080ce101207119d175ea331de832b
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-24 14:30:06 +0200

    Move auth validation data to the src data folder

--- src/data/validation/index.ts
+++ src/data/validation/index.ts
@@ -4,3 +4,36 @@ export const VALIDATION_RULES = {
   password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/,
   isNumber: /^\d+$/,
 } as const;
+
+export const FORM_VALIDATION = {
+  fullName: {
+    required: "First and last Name are required",
+  },
+  email: {
+    required: "Email address is required",
+    pattern: {
+      value: VALIDATION_RULES.isEmail,
+      message: "Invalid email address",
+    },
+  },
+  password: {
+    required: "Password is required",
+    minLength: {
+      value: 8,
+      message: "Password must be at least 8 characters",
+    },
+    pattern: {
+      value: VALIDATION_RULES.password,
+      message: "uppercase, lowercase, number and special",
+    },
+  },
+  mobile: {
+    required: "Phone number is required",
+  },
+  country: {
+    required: "Country is required",
+  },
+  otp: {
+    pattern: VALIDATION_RULES.isNumber,
+  },
+} as const;
