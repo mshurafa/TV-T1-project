ea6977df235eb785733939219f97a3d0d1a88ea1
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-24 23:47:30 +0200

    Create NEW_INVOICE_STEPS constants

---
+++ src/features/invoices/constants/index.ts
@@ -0,0 +1,26 @@
+export const NEW_INVOICE_STEPS = [
+  {
+    title: "1. Confirm Details",
+    id: "step1",
+    active: true,
+    completed: false,
+  },
+  {
+    title: "2. Preview",
+    id: "step2",
+    active: false,
+    completed: false,
+  },
+  {
+    title: "3. Pay",
+    id: "step3",
+    active: false,
+    completed: false,
+  },
+  {
+    title: "4. Confirmation",
+    id: "step4",
+    active: false,
+    completed: false,
+  },
+];
