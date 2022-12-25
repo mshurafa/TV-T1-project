837048286077f12dd1baf92a9a04f5d881baeabf
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-26 01:03:59 +0200

    Convert Stepper component to a compound component

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,41 +1,43 @@
-import React, { useMemo } from "react";
-import Step from "./Step";
-import type { StepperType } from "../types";
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import Content from "./Content";
+import Actions from "./Actions";
+import type { StepperProps, StepperContextType } from "../types";
 
-export const Stepper: StepperType = ({ steps, className, barClassName }) => {
-  const classNames = useMemo(() => {
-    const classes = {
-      // pr-6 same as the width of the bullet
-      stepperContainer: `w-11/12 lg:w-2/5 mx-auto pr-6 ${className ?? ""}`,
-      stepperProgressBar: `bg-gray-200 h-1 flex items-center justify-between ${
-        barClassName ?? ""
-      }`,
-    };
+const StepperContext = React.createContext<StepperContextType>({
+  activeStep: 0,
+  steps: [],
+  onChange: () => {},
+});
 
-    return classes;
-  }, [className, barClassName]);
+export const useStepperContext = () => {
+  const context = React.useContext(StepperContext);
+  if (!context) {
+    throw new Error(
+      `Stepper components cannot be rendered outside the StepperProvider`
+    );
+  }
+  return context;
+};
+
+export const Stepper = ({
+  steps,
+  activeStep,
+  onChange,
+  children,
+}: StepperProps) => {
+  const value = React.useMemo(
+    () => ({ steps, activeStep, onChange }),
+    [steps, activeStep, onChange]
+  );
 
   return (
-    <div className={classNames.stepperContainer}>
-      <div className={classNames.stepperProgressBar}>
-        {steps.map((step, index) => {
-          const isLastBullet = index === steps.length - 2;
-          const isLastStep = index === steps.length - 1;
-          const lastStep = steps[steps.length - 1];
-          if (isLastStep) return null;
-
-          return (
-            <Step
-              key={step.id}
-              step={step}
-              lastStep={lastStep}
-              isLastBullet={isLastBullet}
-            />
-          );
-        })}
-      </div>
-    </div>
+    <StepperContext.Provider value={value}>{children}</StepperContext.Provider>
   );
 };
+
+Stepper.ProgressBar = ProgressBar;
+Stepper.Content = Content;
+Stepper.Actions = Actions;
 
 export default Stepper;
