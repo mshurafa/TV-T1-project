c9ea9d7548079d9aa2d12ec571bc9fe5f6bb5322
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-26 01:32:41 +0200

    Use the Stepper component + useNewInvoice hook inside NewInvoice component

--- src/features/invoices/components/NewInvoice/index.tsx
+++ src/features/invoices/components/NewInvoice/index.tsx
@@ -1,17 +1,18 @@
 import { useMemo } from "react";
-import { Card, Stepper, Button } from "components";
-// import ConfirmDetails from "./ConfirmDetails";
-// import Preview from "./Preview";
+import { Stepper } from "components";
+import ConfirmDetails from "./ConfirmDetails";
+import Preview from "./Preview";
 import Confirmation from "./Confirmation";
-import { NEW_INVOICE_STEPS } from "../../constants";
+import { useNewInvoice } from "../../hooks";
 import type { NewInvoiceType } from "../../types";
 
 export const NewInvoice: NewInvoiceType = ({ className, cardClassName }) => {
-  //create custom hook to manage new invoice state
+  const { steps, activeStep } = useNewInvoice();
+
   const classNames = useMemo(() => {
     const classes = {
-      mainContent: `flex-1 pt-8 ${className ?? ""}`,
-      card: `w-full max-w-[800px] mt-20 mx-auto relative py-8 px-12 ${
+      mainContent: `flex-1 ${className ?? ""}`,
+      card: `w-full max-w-[800px] mx-auto relative py-8 px-12 ${
         cardClassName || ""
       }`,
     };
@@ -19,34 +20,28 @@ export const NewInvoice: NewInvoiceType = ({ className, cardClassName }) => {
     return classes;
   }, [className, cardClassName]);
 
+  const stepsContent = [
+    <ConfirmDetails key="confirm-details" />,
+    <Preview key="preview" />,
+    <div key="payment">Payment</div>,
+    <Confirmation key="confirmation" />,
+  ];
+
   return (
     <div className={classNames.mainContent}>
-      <Stepper steps={NEW_INVOICE_STEPS} />
-      <Card className={classNames.card}>
-        {/* <ConfirmDetails /> */}
-        {/* <Preview /> */}
-        <Confirmation />
-        <div className="flex gap-4 justify-end">
-          <Button
-            type="submit"
-            buttonSize="small"
-            className="max-w-[150px] bg-white text-black border hover:bg-gray-light"
-            fullWidth
-            onClick={() => {}}
-          >
-            Back
-          </Button>
-          <Button
-            type="submit"
-            buttonSize="small"
-            className="max-w-[150px]"
-            fullWidth
-            onClick={() => {}}
-          >
-            Next
-          </Button>
-        </div>
-      </Card>
+      <Stepper
+        steps={steps}
+        activeStep={activeStep}
+        onChange={(stepKey) => {
+          console.log("🚀 ~ file: index.tsx:68 ~ stepKey", stepKey);
+        }}
+      >
+        <Stepper.ProgressBar className="mt-6 mb-20" />
+        <Stepper.Content className={classNames.card}>
+          {stepsContent[activeStep]}
+          <Stepper.Actions />
+        </Stepper.Content>
+      </Stepper>
     </div>
   );
 };
