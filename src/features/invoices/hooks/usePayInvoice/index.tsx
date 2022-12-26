6ba69cbf776cb6ab4426867cb5f469921fa44267
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-27 01:56:45 +0200

    Add nextActionHandler and backActionHandler logic inside usePayInvoice hook

--- src/features/invoices/hooks/usePayInvoice/index.tsx
+++ src/features/invoices/hooks/usePayInvoice/index.tsx
@@ -4,19 +4,75 @@ import Preview from "../../components/PayInvoice/Preview";
 import Confirmation from "../../components/PayInvoice/Confirmation";
 import { PAY_INVOICE_STEPS } from "../../data";
 import type { StepperOnChangeType } from "components/types";
-import type { ConfirmDetailsInputsType } from "../../types";
+import type { ConfirmDetailsInputsType, PayInvoiceStepType } from "../../types";
 
 export const usePayInvoice = () => {
   const [steps, setSteps] = useState(PAY_INVOICE_STEPS);
-  const activeStep = steps.findIndex((step) => step.active);
   const confirmDetailsFormRef = useRef<HTMLButtonElement>(null);
+  const activeStep = steps.findIndex((step) => step.active);
+  const currentStep = steps[activeStep];
+  const nextStep = steps[activeStep + 1];
+  const previousStep = steps[activeStep - 1];
+
+  const nextActionHandler = () => {
+    const updatedCurrentStep: PayInvoiceStepType = {
+      ...currentStep,
+      completed: true,
+      active: false,
+    };
+    const updatedNextStep: PayInvoiceStepType = {
+      ...nextStep,
+      active: true,
+    };
+    const updatedSteps = steps.map((step) => {
+      if (step.id === currentStep.id) {
+        return updatedCurrentStep;
+      } else if (step.id === nextStep.id) {
+        return updatedNextStep;
+      } else {
+        return step;
+      }
+    });
+    setSteps(updatedSteps);
+  };
+
+  const backActionHandler = () => {
+    const updatedStep: PayInvoiceStepType = {
+      ...currentStep,
+      active: false,
+    };
+    const updatedPreviousStep: PayInvoiceStepType = {
+      ...previousStep,
+      active: true,
+    };
+    const updatedSteps = steps.map((step) => {
+      if (step.id === currentStep.id) {
+        return updatedStep;
+      } else if (step.id === previousStep.id) {
+        return updatedPreviousStep;
+      } else {
+        return step;
+      }
+    });
+    setSteps(updatedSteps);
+  };
 
   const onSubmitConfirmDetails = (data: ConfirmDetailsInputsType) => {
     // maybe call API to send the data then store it inside the local storage to save the user progress
-    console.log(
-      "🚀 ~ file: index.tsx:14 ~ onSubmitConfirmDetails ~ data",
-      data
-    );
+    nextActionHandler();
+  };
+
+  const onStepperChange: StepperOnChangeType = (action) => {
+    if (action === "next") {
+      if (currentStep.id === "step1") {
+        confirmDetailsFormRef.current?.click();
+      } else {
+        nextActionHandler();
+      }
+    } else {
+      backActionHandler();
+      console.log("back to the previous step");
+    }
   };
 
   const stepsContent = useMemo(
@@ -30,39 +86,10 @@ export const usePayInvoice = () => {
       <div key="step3">Payment</div>,
       <Confirmation key="step4" />,
     ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
   const stepContent = stepsContent[activeStep];
-
-  const onStepperChange: StepperOnChangeType = (action) => {
-    if (action === "next") {
-      const currentStep = steps[activeStep];
-      if (currentStep.id === "step1") {
-        confirmDetailsFormRef.current?.click();
-      }
-    } else {
-    }
-    // // const activeStep = steps.find((step) => step.active);
-    // console.log(
-    //   "🚀 ~ file: index.tsx:25 ~ onNextClick ~ activeStep",
-    //   activeStep
-    // );
-    // const stepsClone = [...steps];
-    // const updatedSteps = stepsClone.map((step) => {
-    //   const stepClone = { ...step };
-    //   if (step.active) {
-    //     console.log("🚀 ~ file: index.tsx:33 ~ updatedSteps ~ step", step);
-    //     stepClone.active = false;
-    //     stepClone.completed = true;
-    //   }
-    //   return stepClone;
-    // });
-    // console.log(
-    //   "🚀 ~ file: index.tsx:36 ~ updatedSteps ~ updatedSteps",
-    //   updatedSteps
-    // );
-    // setSteps(updatedSteps);
-  };
 
   useEffect(() => {
     () => {
