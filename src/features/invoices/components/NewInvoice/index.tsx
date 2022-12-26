f0abaa55658056097c02062617044613d56ca551
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-26 02:46:08 +0200

    Add the logic for onStepperChange + onSubmitConfirmDetails

--- src/features/invoices/components/NewInvoice/index.tsx
+++ src/features/invoices/components/NewInvoice/index.tsx
@@ -1,13 +1,10 @@
 import { useMemo } from "react";
 import { Stepper } from "components";
-import ConfirmDetails from "./ConfirmDetails";
-import Preview from "./Preview";
-import Confirmation from "./Confirmation";
 import { useNewInvoice } from "../../hooks";
 import type { NewInvoiceType } from "../../types";
 
 export const NewInvoice: NewInvoiceType = ({ className, cardClassName }) => {
-  const { steps, activeStep } = useNewInvoice();
+  const { steps, activeStep, stepContent, onStepperChange } = useNewInvoice();
 
   const classNames = useMemo(() => {
     const classes = {
@@ -20,25 +17,12 @@ export const NewInvoice: NewInvoiceType = ({ className, cardClassName }) => {
     return classes;
   }, [className, cardClassName]);
 
-  const stepsContent = [
-    <ConfirmDetails key="confirm-details" />,
-    <Preview key="preview" />,
-    <div key="payment">Payment</div>,
-    <Confirmation key="confirmation" />,
-  ];
-
   return (
     <div className={classNames.mainContent}>
-      <Stepper
-        steps={steps}
-        activeStep={activeStep}
-        onChange={(stepKey) => {
-          console.log("🚀 ~ file: index.tsx:68 ~ stepKey", stepKey);
-        }}
-      >
+      <Stepper steps={steps} activeStep={activeStep} onChange={onStepperChange}>
         <Stepper.ProgressBar className="mt-6 mb-20" />
         <Stepper.Content className={classNames.card}>
-          {stepsContent[activeStep]}
+          {stepContent}
           <Stepper.Actions />
         </Stepper.Content>
       </Stepper>
--- src/features/invoices/hooks/useNewInvoice/index.tsx
+++ src/features/invoices/hooks/useNewInvoice/index.tsx
@@ -1,32 +1,68 @@
-import { useState, useEffect } from "react";
+import { useState, useMemo, useEffect, useRef } from "react";
+import ConfirmDetails from "../../components/NewInvoice/ConfirmDetails";
+import Preview from "../../components/NewInvoice/Preview";
+import Confirmation from "../../components/NewInvoice/Confirmation";
 import { NEW_INVOICE_STEPS } from "../../constants";
+import type { StepperOnChangeType } from "components/types";
+import type { ConfirmDetailsInputsType } from "../../types";
 
 export const useNewInvoice = () => {
   const [steps, setSteps] = useState(NEW_INVOICE_STEPS);
   const activeStep = steps.findIndex((step) => step.active);
+  const confirmDetailsFormRef = useRef<HTMLButtonElement>(null);
 
-  // const onNextClick = (stepId: string) => {
-  //   const activeStep = steps.find((step) => step.active);
-  //   console.log(
-  //     "🚀 ~ file: index.tsx:25 ~ onNextClick ~ activeStep",
-  //     activeStep
-  //   );
-  //   const stepsClone = [...steps];
-  //   const updatedSteps = stepsClone.map((step) => {
-  //     const stepClone = { ...step };
-  //     if (step.active) {
-  //       console.log("🚀 ~ file: index.tsx:33 ~ updatedSteps ~ step", step);
-  //       stepClone.active = false;
-  //       stepClone.completed = true;
-  //     }
-  //     return stepClone;
-  //   });
-  //   console.log(
-  //     "🚀 ~ file: index.tsx:36 ~ updatedSteps ~ updatedSteps",
-  //     updatedSteps
-  //   );
-  //   setSteps(updatedSteps);
-  // };
+  const onSubmitConfirmDetails = (data: ConfirmDetailsInputsType) => {
+    // maybe call API to send the data then store it inside the local storage to save the user progress
+    console.log(
+      "🚀 ~ file: index.tsx:14 ~ onSubmitConfirmDetails ~ data",
+      data
+    );
+  };
+
+  const stepsContent = useMemo(
+    () => [
+      <ConfirmDetails
+        key="step1"
+        ref={confirmDetailsFormRef}
+        onSubmit={onSubmitConfirmDetails}
+      />,
+      <Preview key="step2" />,
+      <div key="step3">Payment</div>,
+      <Confirmation key="step4" />,
+    ],
+    []
+  );
+  const stepContent = stepsContent[activeStep];
+
+  const onStepperChange: StepperOnChangeType = (action) => {
+    if (action === "next") {
+      const currentStep = steps[activeStep];
+      if (currentStep.id === "step1") {
+        confirmDetailsFormRef.current?.click();
+      }
+    } else {
+    }
+    // // const activeStep = steps.find((step) => step.active);
+    // console.log(
+    //   "🚀 ~ file: index.tsx:25 ~ onNextClick ~ activeStep",
+    //   activeStep
+    // );
+    // const stepsClone = [...steps];
+    // const updatedSteps = stepsClone.map((step) => {
+    //   const stepClone = { ...step };
+    //   if (step.active) {
+    //     console.log("🚀 ~ file: index.tsx:33 ~ updatedSteps ~ step", step);
+    //     stepClone.active = false;
+    //     stepClone.completed = true;
+    //   }
+    //   return stepClone;
+    // });
+    // console.log(
+    //   "🚀 ~ file: index.tsx:36 ~ updatedSteps ~ updatedSteps",
+    //   updatedSteps
+    // );
+    // setSteps(updatedSteps);
+  };
 
   useEffect(() => {
     () => {
@@ -34,7 +70,7 @@ export const useNewInvoice = () => {
     };
   }, []);
 
-  return { steps, activeStep };
+  return { steps, activeStep, stepContent, onStepperChange };
 };
 
 export default useNewInvoice;
