5d810431f1a86332521b5e1f2384e0654dc37d34
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-29 23:07:34 +0200

    Add PAYMENT_METHODS + CLIENT_FEES constants

--- src/features/invoices/data/constants/index.ts
+++ src/features/invoices/data/constants/index.ts
@@ -1,4 +1,8 @@
-import type { PayInvoiceStepsType } from "../../types";
+import type {
+  PayInvoiceStepsType,
+  PaymentMethodType,
+  ClientFeesType,
+} from "../../types";
 
 export const PAY_INVOICE_STEPS: PayInvoiceStepsType = [
   {
@@ -24,5 +28,53 @@ export const PAY_INVOICE_STEPS: PayInvoiceStepsType = [
     id: "step4",
     active: false,
     completed: false,
+  },
+];
+
+export const PAYMENT_METHODS: PaymentMethodType[] = [
+  {
+    id: 1,
+    label: "Credit Card",
+    caption: "Fees 6-8%",
+    value: "creditCard",
+    icon: "/assets/img/debit-card.png",
+  },
+  {
+    id: 2,
+    label: "Paypal",
+    caption: "Fees 3-5%",
+    value: "paypal",
+    icon: "/assets/img/paypal.png",
+  },
+  {
+    id: 3,
+    label: "Bank Transfer",
+    caption: "Fees 12-14%",
+    value: "bankTransfer",
+    icon: "/assets/img/bank.png",
+  },
+];
+
+export const CLIENT_FEES: ClientFeesType[] = [
+  {
+    id: 1,
+    label: "%100 of Fees",
+    caption: "$20",
+    value: 100,
+    icon: "/assets/img/happy.png",
+  },
+  {
+    id: 2,
+    label: "%50 of Fees",
+    caption: "$10",
+    value: 50,
+    icon: "/assets/img/happy-face.png",
+  },
+  {
+    id: 3,
+    label: "%0 of Fees",
+    caption: "$0",
+    value: 0,
+    icon: "/assets/img/sad-face.png",
   },
 ];
