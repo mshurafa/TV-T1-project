11d352fad6879ed5932c9a3b79bc6196086ad3d5
MohJaber93 <mohjbr.dev@gmail.com>
2022-12-31 01:08:36 +0200

    Call API conditionally if the id has value

--- src/features/invoices/hooks/useInvoiceDetails/index.tsx
+++ src/features/invoices/hooks/useInvoiceDetails/index.tsx
@@ -6,9 +6,9 @@ import type { InvoiceDetailsResponse } from "../../types";
 const invoiceDetailsFetcher: Fetcher<InvoiceDetailsResponse, string> = (url) =>
   axios.get(url).then((res) => res.data);
 
-export const useInvoiceDetails = (id: string) => {
+export const useInvoiceDetails = (id: string | undefined) => {
   const { data, error, isLoading } = useSWR(
-    API_SERVICES_URLS.CLIENT.INVOICE(id),
+    id ? API_SERVICES_URLS.CLIENT.INVOICE(id) : null,
     invoiceDetailsFetcher
   );
   return { invoice: data, error, isLoading };
