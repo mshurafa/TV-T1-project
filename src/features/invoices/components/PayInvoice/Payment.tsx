3596966a81e25b972a6b83d4814f01185c80b849
MohJaber93 <mohjbr.dev@gmail.com>
2023-01-08 01:17:01 +0200

    Use ToggleButtons component in payment step

--- src/features/invoices/components/PayInvoice/Payment.tsx
+++ src/features/invoices/components/PayInvoice/Payment.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Divider, Button, Image } from "components";
+import { Divider, Image, ToggleButtons } from "components";
 import { PAYMENT_METHODS, CLIENT_FEES } from "../../data";
-import { PaymentMethodValue, ClientFeesValue } from "../../types";
+import type { PaymentMethodValue, ClientFeesValue } from "../../types";
 
 const Payment = () => {
   const [paymentMethod, setPaymentMethod] = useState<PaymentMethodValue | "">(
@@ -9,59 +9,54 @@ const Payment = () => {
   );
   const [clientFee, setClientFee] = useState<ClientFeesValue>();
 
-  const buttonClassName =
-    "sm:shadow-none sm:hover:shadow-none font-medium text-sm px-2 hover:bg-gray-100 active:bg-gray-100 outline sm:text-gray-400";
-  const activeButtonClassName =
-    "sm:outline-gray-400 sm:bg-white hover:bg-white sm:text-black";
-
   return (
     <>
       <h2 className="text-lg font-medium">Choose Payment Method</h2>
       <Divider />
       <div className="my-6 flex p-1 gap-3">
-        {PAYMENT_METHODS.map((method) => (
-          <Button
-            key={method.id}
-            onClick={() => setPaymentMethod(method.value)}
-            className={`flex-1 bg-transparent text-black hover:bg-gray-100 border ${
-              paymentMethod === method.value
-                ? "opacity-100 border-blue"
-                : "opacity-50"
-            }`}
-          >
-            <span className="flex items-center">
-              <Image
-                alt={method.label}
-                src={method.icon}
-                width={33}
-                height={33}
-                className={method.value === "bankTransfer" ? "opacity-50" : ""}
-              />
-              <span className="flex-1 text-left pl-5 text-sm">
-                {method.label}
-                <span className="block text-xs">{method.caption}</span>
+        <ToggleButtons
+          value={paymentMethod}
+          onChange={(value) => setPaymentMethod(value as PaymentMethodValue)}
+        >
+          {PAYMENT_METHODS.map((method) => (
+            <ToggleButtons.Button key={method.id} value={method.value}>
+              <span className="flex items-center">
+                <Image
+                  alt={method.label}
+                  src={method.icon}
+                  width={33}
+                  height={33}
+                  className={
+                    method.value === "bankTransfer" ? "opacity-50" : ""
+                  }
+                />
+                <span className="flex-1 text-left pl-5 text-sm">
+                  {method.label}
+                  <span className="block text-xs">{method.caption}</span>
+                </span>
               </span>
-            </span>
-          </Button>
-        ))}
+            </ToggleButtons.Button>
+          ))}
+        </ToggleButtons>
       </div>
-      {paymentMethod && (
-        <>
-          <h2 className="text-lg font-medium">
-            Empower a Freelancer by Helping with Fees
-          </h2>
-          <Divider />
-          <div className="my-6 flex p-1 gap-3">
+      <div
+        className={`${
+          !paymentMethod
+            ? "max-h-0"
+            : "max-h-[500px] transition-all duration-[1500ms]"
+        } overflow-hidden`}
+      >
+        <h2 className="text-lg font-medium">
+          Empower a Freelancer by Helping with Fees
+        </h2>
+        <Divider />
+        <div className="my-6 flex p-1 gap-3">
+          <ToggleButtons
+            value={`${clientFee}`}
+            onChange={(value) => setClientFee(+value as ClientFeesValue)}
+          >
             {CLIENT_FEES.map((fee) => (
-              <Button
-                key={fee.id}
-                onClick={() => setClientFee(fee.value)}
-                className={`flex-1 bg-transparent text-black hover:bg-gray-100 border ${
-                  clientFee === fee.value
-                    ? "opacity-100 border-blue"
-                    : "opacity-50"
-                }`}
-              >
+              <ToggleButtons.Button key={fee.id} value={`${fee.value}`}>
                 <span className="flex items-center">
                   <span className="flex-1 text-left pl-5 text-sm">
                     {fee.label}
@@ -74,12 +69,12 @@ const Payment = () => {
                     height={33}
                   />
                 </span>
-              </Button>
+              </ToggleButtons.Button>
             ))}
-          </div>
-          <span className="text-sm">Freelancer Gets $500</span>
-        </>
-      )}
+          </ToggleButtons>
+        </div>
+        <span className="text-sm">Freelancer Gets $500</span>
+      </div>
     </>
   );
 };
