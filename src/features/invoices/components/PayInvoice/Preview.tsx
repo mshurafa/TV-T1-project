7860f619f5c29cb879db49ae471d41a9b5a6c71a
MohJaber93 <mohjbr.dev@gmail.com>
2023-01-07 23:35:23 +0200

    Use useInvoicePreview hook and render invoice preview data

--- src/features/invoices/components/PayInvoice/Preview.tsx
+++ src/features/invoices/components/PayInvoice/Preview.tsx
@@ -1,11 +1,30 @@
-import { Card, Logo } from "components";
+import { Card, Logo, Skeleton } from "components";
+import { getFullName } from "utils";
+import { usePayInvoice } from "../../contexts/PayInvoice";
+import { useInvoicePreview } from "../../hooks";
 
 const Preview = () => {
+  const { invoiceId } = usePayInvoice();
+  const { preview } = useInvoicePreview(invoiceId);
+  const clientFullName = preview
+    ? getFullName(preview.client.firstName, preview.client.lastName)
+    : "";
+
   return (
     <Card className="mb-4 shadow-sm border py-8 px-11">
       <div className="flex justify-between items-center">
         <h4 className="text-lg font-medium">
-          Invoice <span className="text-xs font-normal">#INV-003</span>
+          Invoice{" "}
+          <span className="text-xs font-normal">
+            #
+            {preview?.invoiceNo || (
+              <Skeleton
+                width={49}
+                height={14}
+                className="inline-block align-text-bottom"
+              />
+            )}
+          </span>
         </h4>
         <Logo className="cursor-pointer" />
       </div>
@@ -22,35 +41,76 @@ const Preview = () => {
         </div>
         <div>
           <h5 className="text-gray-dark mt-5 mb-4">Bill To</h5>
-          <p>John Doe</p>
-          <span className="text-gray-dark text-sm">Email@mail.com</span>
+          <p>{clientFullName ? clientFullName : <Skeleton width={150} />}</p>
+          <span className="text-gray-dark text-sm">
+            {preview?.client.email || <Skeleton width={130} className="mt-1" />}
+          </span>
           <p className="mt-3">Issue Date</p>
-          <span className="text-gray-dark text-sm">July 27 ,2022</span>
+          <span className="text-gray-dark text-sm">
+            {preview ? (
+              new Date(preview.createdAt).toDateString()
+            ) : (
+              <Skeleton width={110} />
+            )}
+          </span>
         </div>
       </div>
       <div className="flex justify-between items-start mt-14">
-        <div>
-          <p className="text-gray-dark mb-2">Service</p>
-          <span>UI/UX Design for Noon Website</span>
-        </div>
-        <div>
-          <p className="text-gray-dark mb-2">Amount</p>
-          <span>$ 300</span>
-        </div>
+        <p className="text-gray-dark mb-2">Service</p>
+        <p className="text-gray-dark mb-2">Amount</p>
       </div>
+      {preview ? (
+        preview?.fixed.map((item) => (
+          <div key={item._id} className="flex justify-between items-start">
+            <span>{item.itemName}</span>
+            <span>
+              {preview.currency} {item.price}
+            </span>
+          </div>
+        ))
+      ) : (
+        <div className="flex justify-between items-start">
+          <span>
+            <Skeleton width={160} />
+          </span>
+          <span>
+            <Skeleton width={60} />
+          </span>
+        </div>
+      )}
       <div className="h-px my-3 bg-gray" />
-      <div className="ml-auto max-w-max min-w-[150px] px-4 text-gray-dark text-sm">
+      <div className="ml-auto max-w-max min-w-[160px] text-gray-dark text-sm">
         <p className="flex">
           Sub Total
-          <span className="ml-auto">$..</span>
+          <span className="ml-auto">
+            {preview ? (
+              `${preview?.currency} ${preview.subTotal}`
+            ) : (
+              <Skeleton width={60} />
+            )}
+          </span>
         </p>
         <p className="flex">
           Fees
-          <span className="ml-auto">$..</span>
+          <span className="ml-auto">
+            {preview ? (
+              `${preview?.currency} ${preview?.subTotal
+                .toString()
+                .replaceAll(/[0-9]/g, "0")}`
+            ) : (
+              <Skeleton width={60} />
+            )}
+          </span>
         </p>
         <p className="flex">
           Total
-          <span className="ml-auto">$..</span>
+          <span className="ml-auto">
+            {preview ? (
+              `${preview?.currency} ${preview.subTotal}`
+            ) : (
+              <Skeleton width={60} />
+            )}
+          </span>
         </p>
       </div>
     </Card>
