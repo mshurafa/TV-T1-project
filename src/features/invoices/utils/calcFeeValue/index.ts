d5db429fc1d54c6d33da6e053b2e4a1f0a3181fc
MohJaber93 <mohjbr.dev@gmail.com>
2023-01-13 00:55:32 +0200

    Adjust calcFeeValue to adapt the new ClientFeesValue type

--- src/features/invoices/utils/calcFeeValue/index.ts
+++ src/features/invoices/utils/calcFeeValue/index.ts
@@ -4,22 +4,5 @@ export const calcFeeValue = (
   value: ClientFeesValue,
   paymentOption: PaymentOptionFee | undefined
 ) => {
-  let fee: number | undefined;
-
-  switch (value) {
-    case 0: {
-      fee = paymentOption?.none;
-      break;
-    }
-    case 50: {
-      fee = paymentOption?.halfFee;
-      break;
-    }
-    case 100: {
-      fee = paymentOption?.fullFee;
-      break;
-    }
-  }
-
-  return fee;
+  return paymentOption?.[value];
 };
