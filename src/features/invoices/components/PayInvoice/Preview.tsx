8fa7458da483a900312fdf8a9e23ce9fa5954409
MohJaber93 <mohjbr.dev@gmail.com>
2023-01-21 19:06:15 +0200

    Export PayInvoice components as defaults

--- src/features/invoices/components/PayInvoice/Preview.tsx
+++ src/features/invoices/components/PayInvoice/Preview.tsx
@@ -3,7 +3,7 @@ import { getFullName } from "utils";
 import { usePayInvoice } from "../../contexts/PayInvoice";
 import { useInvoicePreview } from "../../hooks";
 
-export const Preview = () => {
+const Preview = () => {
   const { invoiceId } = usePayInvoice();
   const { preview } = useInvoicePreview(invoiceId);
   const clientFullName = preview
@@ -11,8 +11,8 @@ export const Preview = () => {
     : "";
 
   return (
-    <Card className="mb-4 shadow-sm border py-8 px-11">
-      <div className="flex justify-between items-center">
+    <Card className="py-8 mb-4 border shadow-sm px-11">
+      <div className="flex items-center justify-between">
         <h4 className="text-lg font-medium">
           Invoice{" "}
           <span className="text-xs font-normal">
@@ -30,9 +30,9 @@ export const Preview = () => {
       </div>
       <div className="flex justify-between">
         <div>
-          <h5 className="text-gray-dark mt-5 mb-4">From</h5>
+          <h5 className="mt-5 mb-4 text-gray-dark">From</h5>
           <h6>Talents Valley LLC</h6>
-          <p className="text-gray-dark text-sm flex flex-col">
+          <p className="flex flex-col text-sm text-gray-dark">
             <span>30 North Gould St.</span>
             <span>Sheridan, Wyoming 82801</span>
             <span>United States</span>
@@ -40,13 +40,13 @@ export const Preview = () => {
           </p>
         </div>
         <div>
-          <h5 className="text-gray-dark mt-5 mb-4">Bill To</h5>
+          <h5 className="mt-5 mb-4 text-gray-dark">Bill To</h5>
           <p>{clientFullName ? clientFullName : <Skeleton width={150} />}</p>
-          <span className="text-gray-dark text-sm">
+          <span className="text-sm text-gray-dark">
             {preview?.client.email || <Skeleton width={130} className="mt-1" />}
           </span>
           <p className="mt-3">Issue Date</p>
-          <span className="text-gray-dark text-sm">
+          <span className="text-sm text-gray-dark">
             {preview ? (
               new Date(preview.createdAt).toDateString()
             ) : (
@@ -55,13 +55,13 @@ export const Preview = () => {
           </span>
         </div>
       </div>
-      <div className="flex justify-between items-start mt-14">
-        <p className="text-gray-dark mb-2">Service</p>
-        <p className="text-gray-dark mb-2">Amount</p>
+      <div className="flex items-start justify-between mt-14">
+        <p className="mb-2 text-gray-dark">Service</p>
+        <p className="mb-2 text-gray-dark">Amount</p>
       </div>
       {preview ? (
         preview?.fixed.map((item) => (
-          <div key={item._id} className="flex justify-between items-start">
+          <div key={item._id} className="flex items-start justify-between">
             <span>{item.itemName}</span>
             <span>
               {preview.currency} {item.price}
@@ -69,7 +69,7 @@ export const Preview = () => {
           </div>
         ))
       ) : (
-        <div className="flex justify-between items-start">
+        <div className="flex items-start justify-between">
           <span>
             <Skeleton width={160} />
           </span>
--- src/features/invoices/components/index.ts
+++ src/features/invoices/components/index.ts
@@ -1,4 +1,4 @@
-export * from "./PayInvoice/ConfirmDetails";
-export * from "./PayInvoice/Preview";
+export { default as ConfirmDetails } from "./PayInvoice/ConfirmDetails";
+export { default as Preview } from "./PayInvoice/Preview";
 export { default as Payment } from "./PayInvoice/Payment";
 export { default as Confirmation } from "./PayInvoice/Confirmation";
