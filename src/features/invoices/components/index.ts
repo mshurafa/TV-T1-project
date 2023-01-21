55a5e4485cd1e4838b73497945dceb4deca5f530
MohJaber93 <mohjbr.dev@gmail.com>
2023-01-21 19:02:28 +0200

    Export Payment + Confirmation components

--- src/features/invoices/components/index.ts
+++ src/features/invoices/components/index.ts
@@ -1,2 +1,4 @@
 export * from "./PayInvoice/ConfirmDetails";
 export * from "./PayInvoice/Preview";
+export { default as Payment } from "./PayInvoice/Payment";
+export { default as Confirmation } from "./PayInvoice/Confirmation";
