792a07ca916355d23aaeffc362493b3a97ec7cf6
saadyasser <saad.wadi.229@gmail.com>
2023-03-13 17:09:29 +0200

    test

---
+++ src/pages/page2/index.tsx
@@ -0,0 +1,13 @@
+import { Card } from "components";
+const WithdrowForm = () => {
+  return (
+    <>
+      <Card className="w-5/12 mx-auto ">
+        <div>
+          <h3>Choose Payment Method</h3> <p>test</p>
+        </div>
+      </Card>
+    </>
+  );
+};
+export default WithdrowForm;
