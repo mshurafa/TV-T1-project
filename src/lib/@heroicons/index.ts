eb69f7b314ebb3d41b55203c95b819ff95677f95
Mohammed Shurafa <shorafahamod@gmail.com>
2023-03-13 11:52:40 +0200

    add home page with sidebar

--- src/lib/@heroicons/index.ts
+++ src/lib/@heroicons/index.ts
@@ -1,25 +1,37 @@
 import {
-  ExclamationTriangleIcon as ErrorIconMini,
-  CheckCircleIcon as CheckCircleIconMini,
-  EyeIcon as EyeIconMini,
-  EyeSlashIcon as EyeSlashIconMini,
-  ChevronLeftIcon as ChevronLeftIconMini,
-  ArrowUpTrayIcon as ArrowUpTrayIconMini,
-  XMarkIcon as XMarkIconMini,
-  InformationCircleIcon as InformationCircleIconMini,
-  CheckIcon as CheckIconMini,
+    ExclamationTriangleIcon as ErrorIconMini,
+    CheckCircleIcon as CheckCircleIconMini,
+    EyeIcon as EyeIconMini,
+    EyeSlashIcon as EyeSlashIconMini,
+    ChevronLeftIcon as ChevronLeftIconMini,
+    ArrowUpTrayIcon as ArrowUpTrayIconMini,
+    XMarkIcon as XMarkIconMini,
+    InformationCircleIcon as InformationCircleIconMini,
+    CheckIcon as CheckIconMini,
 } from "@heroicons/react/20/solid";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import {
+    InformationCircleIcon,
+    HomeIcon as Home,
+    Bars3Icon as MenuBar,
+    ArrowRightOnRectangleIcon as LogOut,
+    Cog6ToothIcon as Settings,
+    QuestionMarkCircleIcon as Help,
+    ChatBubbleLeftRightIcon as Contacts,
+    BanknotesIcon as Withdraw,
+    ClipboardDocumentCheckIcon as Invoices
+
+} from "@heroicons/react/24/outline";
 
 export {
-  ErrorIconMini,
-  CheckCircleIconMini,
-  EyeIconMini,
-  EyeSlashIconMini,
-  ChevronLeftIconMini,
-  ArrowUpTrayIconMini,
-  XMarkIconMini,
-  InformationCircleIconMini,
-  InformationCircleIcon,
-  CheckIconMini,
+    ErrorIconMini,
+    CheckCircleIconMini,
+    EyeIconMini,
+    EyeSlashIconMini,
+    ChevronLeftIconMini,
+    ArrowUpTrayIconMini,
+    XMarkIconMini,
+    InformationCircleIconMini,
+    InformationCircleIcon,
+    CheckIconMini,
+    Home, MenuBar, LogOut, Settings, Help, Contacts, Withdraw, Invoices
 };
---
+++ src/pages/home/homeImages.js
@@ -0,0 +1,15 @@
+import arrow from '../../../public/assets/img/curved-arrow@2x.png'
+import send from '../../../public/assets/img/send@2x.png'
+import withdraw from '../../../public/assets/img/Withdraw@2x.png'
+import CreateInvoice from '../../../public/assets/img/CreateInvoice.png'
+import ClientPaysIt from '../../../public/assets/img/ClientPaysIt.png'
+import GetPaid from '../../../public/assets/img/GetPaid.png'
+
+export const images = {
+    arrow,
+    send,
+    withdraw,
+    CreateInvoice,
+    ClientPaysIt,
+    GetPaid
+}
