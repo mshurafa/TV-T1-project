d8fcaf94d79ac6a31947611bd668a72bf0b54907
Mohammed Shurafa <shorafahamod@gmail.com>
2023-03-16 16:52:32 +0200

    ignoreBuildErrors

--- src/pages/Balance/SampleAPI.js
+++ src/pages/Balance/SampleAPI.js
@@ -1,4 +1,4 @@
-export const withdrawalData = {
+const withdrawalData = {
     getWithdrawalData() {
         return [
             {
--- src/pages/home/homeImages.js
+++ src/pages/home/homeImages.js
@@ -5,7 +5,7 @@ import CreateInvoice from '../../../public/assets/img/CreateInvoice.png'
 import ClientPaysIt from '../../../public/assets/img/ClientPaysIt.png'
 import GetPaid from '../../../public/assets/img/GetPaid.png'
 
-export const images = {
+const images = {
     arrow,
     send,
     withdraw,
